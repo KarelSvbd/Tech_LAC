a3fc0e567afa82fb0abbf4f86d2546fcfe25990d
KarelSvbd <57522089+KarelSvbd@users.noreply.github.com>
2023-01-16 20:57:25 +0100

    v. 0.1 busybox

---
+++ exC/my_busybox.c
@@ -0,0 +1,97 @@
+/// \file maze.c
+/// \brief Replica of the os busybox in c
+/// \author Karel.SVBD, CFPT-i
+/// \date 16.01.2023
+/// \version 0.1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//For main
+#define MIN_MAIN_ARGUMENTS 1
+#define MY_SEQ_NAME "my_seq"
+#define MY_RANDOM_NAME "my_random"
+
+//For my_seq
+#define INC_DEFAULT_VALUE 1
+#define LIMIT_ARGUMENT 4
+#define MIN_ARGUMENTS_SEQ 3
+
+//For my_random
+#define ARGUMENTS_RANDOM 3
+
+//For both
+#define MARGIN 1
+#define FIRST_ARGUMENT 2
+#define SECOND_ARGUMENT 3
+
+//Creates a sequence using the arguments
+//Argv[0] filename
+//Argv[1] min
+//Argv[3] (optional) interval
+//Argv[2] max
+void my_seq(int argc, char *argv[]){
+
+    if(argc >= MIN_ARGUMENTS_SEQ)
+    {
+        //getting the first argument
+        int i = atoi(argv[FIRST_ARGUMENT]);
+    
+        int inc = INC_DEFAULT_VALUE;
+    
+        if(argc == LIMIT_ARGUMENT + MARGIN){
+            inc = atoi(argv[SECOND_ARGUMENT]);
+        }
+
+        //While i is not bigger then the last argument
+        while(i <= atoi(argv[argc - MARGIN])){
+            printf("%d\n", i);
+            i += inc; 
+        }
+    }
+    else{
+        printf("%s%d%s%d\n", "Wrong number of arguments. Given : ", argc, " asked min : ", MIN_ARGUMENTS_SEQ);
+    }
+}
+
+//Creates a sequence using the arguments
+//Random source : https://www.geeksforgeeks.org/generating-random-number-range-c/
+//Argv[0] filename
+//Argv[1] min (included)
+//Argv[2] max (included)
+void my_random(int argc, char *argv[]){
+
+    if(argc == ARGUMENT_RANDOM)
+    {
+        int firstArguement = atoi(argv[FIRST_ARGUMENT]);
+        int secondArguement = atoi(argv[SECOND_ARGUMENT]);
+
+        // seed for random
+        srand(time(0));
+        int result = (rand() % (secondArguement - firstArguement + MARGIN)) + firstArguement;
+        printf("%d\n", result);
+
+    }
+    else{
+        printf("%s%d%s%d\n", "Wrong number of arguments. Given : ", argc, " asked : ", ARGUMENT_RANDOM);
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    if(strcmp(argv[1], MY_SEQ_NAME)){
+        printf("%s\n", "oui");
+    }
+
+    /*//excludion filename with margin
+    if(!(argv == MIN_MAIN_ARGUMENTS - MARGIN)){
+        my_random(argc, argv);
+    }
+    else{
+
+    }*/
+    
+    return 0;
+}
